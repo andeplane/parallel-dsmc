b8bc0df57b05a90f36b11f173f9b8d9ba8b4df76
Anders Hafreager <anders@tapcat.is>
2013-09-23 12:21:14 +0200

    Updated run.py

--- run.py
+++ run.py
@@ -1,37 +1,66 @@
-# This example file shows how to melt an SiO2 system
-
 from dsmcconfig import *
-
-# n{x,y,z} is number of processors in each dimension.
-# unit_cells_{x,y,z} is number of unit cells in each dimension.
-# More parameters in constructor.
+from dsmcplotting import *
+from dsmc_unit_converter import *
+from math import sqrt
 
 program = DSMC()
-dsmc = program.compile(skip_compile=True, name="job1")
+dsmc = program.compile(skip_compile=False, name="job1")
+uc = DSMC_unit_converter(program)
 
 program.reset()
-program.reservoir_fraction = 0.2
-program.atoms_per_molecule = 50000
 
-#program.density = 1e25
-program.wall_temperature = 100
-program.temperature = 100
-program.cells_x = 20
-program.cells_y = 20
-program.cells_z = 20
+program.gravity = 0.1
+program.gravity_direction = 2
+program.reservoir_fraction = 0
+program.atoms_per_molecule = 50
+program.Lx = 0.2
+program.Ly = 0.2
+program.Lz = 1.0
 
+actual_ly = program.Ly*0.8 # The rest is upper and lower wall
+
+program.density = uc.density_from_knudsen_number(knudsen_number=0.1, length=actual_ly)
+program.temperature = 300
+program.cells_x = 12
+program.cells_y = 12
+program.cells_z = 60
+
+program.maintain_pressure = False
 ideal_gas_pressure = program.density*program.constants['boltzmann']*program.temperature;
-program.maintain_pressure = True
-program.pressure_A = ideal_gas_pressure + 50000
+program.pressure_A = ideal_gas_pressure
 program.pressure_B = ideal_gas_pressure
-program.world = "../worlds/box.bin"
-#program.surface_interaction = "cercignani_lampis"
 
-program.prepare_new_system()
-program.run(dsmc)
+program.world = "../worlds/box_no_reservoir.bin"
+program.statistics_interval = 1000
+program.velocity_bins = 80
 
-program.timesteps = 300000
-program.create_config_file()
-program.run(dsmc)
+if True:
+	program.prepare_new_system()
+	program.run(dsmc)
 
-program.run(dsmc)
+	program.timesteps = 5000
+	program.create_config_file()
+	program.run(dsmc)
+	program.save_state(path="states/00_thermalized")
+
+	program.load_state(path="states/00_thermalized")
+	program.statistics_interval = 10
+	program.timesteps = 20000
+
+	program.create_config_file()
+	program.run(dsmc)
+	program.save_state(path="states/01_thermalized")
+
+if False:
+	program.load_state(path="states/01_thermalized")
+	program.create_movie_files = True
+	program.movie_molecules = 8000
+	program.timesteps = 1000
+	program.create_config_file()
+	program.run(dsmc)
+	program.create_movie(frames=1000)
+mass_si = 6.63352088e-26; #kg
+factor = 1.0#/sqrt(2*program.constants['boltzmann']/mass_si*program.temperature)
+
+vis = Visualizer()
+vis.plot_velocity_distribution_box(state_path="states/01_thermalized", height=program.Ly, output="velocity_profile.pdf", show_plot=True, factor=factor)
